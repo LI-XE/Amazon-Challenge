ea64c041a9cfb463538901d276c4bdeb01012e53
LI_XE <hyangeun312@gmail.com>
2022-01-02 00:23:21 -0600

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      div.home_container
+      
+    </div>
+  );
+}
+
+export default Home;
