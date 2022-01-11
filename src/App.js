887e7a8b0726947cac36972f9a95ad5777260459
LI_XE <hyangeun312@gmail.com>
2022-01-10 22:03:34 -0600

    add payment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
@@ -7,6 +7,13 @@ import Checkout from "./components/checkout/Checkout";
 import Login from "./components/reglog/Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./state/StateProvider";
+import Payment from "./components/payment/Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51KGIhFLdOZDJArv24aui5CRwl43qAYze2FeOn1IfkxCnGAxJkRczTb0uYoVTGKtf0dvtbofQY7JfYFzdS4j6SIZU000iOFFmbP"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -37,12 +44,22 @@ function App() {
     // BEM
     <Router>
       <div className="app">
-        <Header />
-        <Routes>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/checkout" element={<Checkout />}></Route>
-        </Routes>
+        <React.Fragment>
+          <Header />
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route
+              path="/payment"
+              element={
+                <Elements stripe={promise}>
+                  <Payment />
+                </Elements>
+              }
+            />
+          </Routes>
+        </React.Fragment>
       </div>
     </Router>
   );
---
+++ src/components/payment/Payment.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from "react";
+import axios from "../../axios";
+import "./Payment.css";
+import { Link, Navigate } from "react-router-dom";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "../../state/StateProvider";
+import { getBasketTotal } from "../../state/reducer";
+import CheckoutProduct from "../checkoutProduct/CheckoutProduct";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  useEffect(() => {
+    // generate the special stripe secret which allows us to charge a customer
+
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        // Stripe expects the total in a currencies subunits
+        url: `/payments/create?total=${getBasketTotal(basket) * 100}`,
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+
+    getClientSecret();
+  }, [basket]);
+
+  const handleSubmit = async (e) => {
+    // do all the fancy stripe things...
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        // paymentIntent = payment confirmation
+
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+
+        Navigate("/orders", { replace: true });
+      });
+  };
+
+  const handleChange = (e) => {
+    // Listen for changes in hte CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
+
+  return (
+    <div className="payment">
+      <div className="payment_container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
+        <div className="payment_section">
+          <div className="payment_title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment_address">
+            <p>{user?.email}</p>
+            <p>12312312323</p>
+            <p>los angeles,CA 32412</p>
+          </div>
+        </div>
+        <div className="payment_section">
+          <div className="payment_title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment_items">
+            {basket?.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="payment_section">
+          <div className="payment_title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment_details">
+            {/* Stripe */}
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+
+              <div className="payment_priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => <h3>Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
+
+              {/* Error */}
+              {error && <div>{error}</div>}
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
